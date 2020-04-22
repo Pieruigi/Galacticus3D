520c239ea8bfb5ca3d0ffbc25c743fa7d849647e
Pierpiero <pierluigi.zomparelli@gmail.com>
2020-04-23 01:20:34 +0200

    Added Unity advanced navmesh system

--- Assets/_TestPathFinding/AI.cs
+++ Assets/_TestPathFinding/AI.cs
@@ -1,26 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
     [SerializeField]
     Transform target;
 
-    AIPath aiPath;
+    NavMeshAgent agent;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        aiPath.destination = target.position;
+        agent.SetDestination(target.position);
     }
 }
