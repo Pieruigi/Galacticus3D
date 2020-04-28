ed7a99b2b10a900dd1ef2be5f7d9280b38fb809c
Pierpiero <pierluigi.zomparelli@gmail.com>
2020-04-29 00:13:29 +0200

    Enemy go back on disengage; added health ui for testing

---
+++ Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OMTB.UI
+{
+    public class HealthUI : MonoBehaviour
+    {
+        [SerializeField]
+        Image bar;
+
+        Health health;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+            health.OnHealthChanged += HandleOnHealthChanged;
+            bar.fillAmount = health.CurrentHealth / health.MaxHealth;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void HandleOnHealthChanged()
+        {
+            bar.fillAmount = health.CurrentHealth / health.MaxHealth;
+        }
+    }
+
+}
--- Assets/Scripts/Weapons/Health.cs
+++ Assets/Scripts/Weapons/Health.cs
@@ -8,12 +8,21 @@ namespace OMTB
 {
     public class Health : MonoBehaviour, IDamageable
     {
-        
+        public UnityAction OnHealthChanged;
+
         [SerializeField]
         float maxHealth;
+        public float MaxHealth
+        {
+            get { return maxHealth; }
+        }
 
         [SerializeField]
         float health;
+        public float CurrentHealth
+        {
+            get { return health; }
+        }
 
        
         void Awake()
@@ -37,7 +46,13 @@ namespace OMTB
         public void ApplyDamage(float amount)
         {
             health -= amount;
-            if(health < 0)
+
+            if (health < 0)
+                health = 0;
+
+            OnHealthChanged?.Invoke();
+
+            if(health == 0)
             {
                 Destroy(gameObject);
             }
