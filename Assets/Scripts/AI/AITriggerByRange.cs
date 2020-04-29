ea847e6f75be5fba6bd709f4201c3b2aa2a31ce3
Pierpiero <pierluigi.zomparelli@gmail.com>
2020-04-29 23:46:21 +0200

    Optimization improved

--- Assets/Scripts/AI/AITriggerByRange.cs
+++ Assets/Scripts/AI/AITriggerByRange.cs
@@ -109,6 +109,7 @@ namespace OMTB.AI
         }
 
        
+
     }
 }
 
--- Assets/Scripts/Commons/Optimizer.cs
+++ Assets/Scripts/Commons/Optimizer.cs
@@ -8,37 +8,50 @@ namespace OMTB
     {
         GameObject player;
 
-        float range = 80;
         float sqrRange;
 
         Transform[] walls;
 
+        float w, h = 80; 
+
         // Start is called before the first frame update
         void Start()
         {
-            sqrRange = range * range;
+            w = h * Screen.width / Screen.height;
+            w = w / 2f;
+            h = h / 2f;
+            Debug.Log("W:" + w);
+            Debug.Log("H:" + h);
 
             player = GameObject.FindGameObjectWithTag("Player");
             walls = new Transform[transform.childCount];
             
             for(int i=0; i<transform.childCount; i++)
             {
-                Transform child = transform.GetChild(i);
+                Transform child = transform.GetChild(i); 
                 walls[i] = child;
 
+                bool remove = false;
                 // Remove random particle systems ( corresponding light must be inside )
-                bool remove = ((int)Random.Range(0,3) == 0) ? false : true;
-                if (remove)
+                ParticleSystem[] psList = child.GetComponentsInChildren<ParticleSystem>();
+              
+                for(int j=0; j<psList.Length; j++)
                 {
-                    Destroy(child.GetComponentInChildren<ParticleSystem>());
+                    remove = ((int)Random.Range(0, 4) == 0) ? false : true;
+                    if (remove)
+                    {
+                        Destroy(psList[j].gameObject);
+                    }
                 }
+                
 
 
                 // Set some mesh static
-                Rotator[] rotators = child.GetChild(0).GetComponentsInChildren<Rotator>();
+                Rotator[] rotators = child.GetComponentsInChildren<Rotator>();
+                Debug.Log("Rotators.Length:" + rotators.Length);
                 for(int j=0; j<rotators.Length; j++)
                 {
-                    remove = ((int)Random.Range(0, 5) == 0) ? false : true;
+                    remove = ((int)Random.Range(0, 4) == 0) ? false : true;
                     if (remove)
                     {
                         Destroy(rotators[j]);
@@ -59,19 +72,22 @@ namespace OMTB
 
         private void LateUpdate()
         {
+            Vector3 pos = player.transform.position;
+
             for(int i=0; i<walls.Length; i++)
             {
-                if ((player.transform.position - walls[i].position).sqrMagnitude > sqrRange)
+                if(walls[i].position.x > pos.x - w && walls[i].position.x < pos.x + w &&
+                   walls[i].position.z > pos.z - h && walls[i].position.z < pos.z + h)
                 {
-                    if (walls[i].gameObject.activeSelf)
-                        walls[i].gameObject.SetActive(false);
-
+                    if (!walls[i].GetChild(0).gameObject.activeSelf)
+                        walls[i].GetChild(0).gameObject.SetActive(true);
                 }
                 else
                 {
-                    if (!walls[i].gameObject.activeSelf)
-                        walls[i].gameObject.SetActive(true);
+                    if (walls[i].GetChild(0).gameObject.activeSelf)
+                        walls[i].GetChild(0).gameObject.SetActive(false);
                 }
+
                 
 
             }
