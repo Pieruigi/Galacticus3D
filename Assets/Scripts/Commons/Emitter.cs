88f066713d8e12de7d64d3d2ef63231f19a88b43
Pierpiero <pierluigi.zomparelli@gmail.com>
2020-04-30 00:23:46 +0200

    Enemy and bullets color

---
+++ Assets/Scripts/Commons/Emitter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OMTB
+{
+    public class Emitter : MonoBehaviour
+    {
+        [SerializeField]
+        bool isEnemy = false;
+        public bool IsEnemy
+        {
+            get { return isEnemy; }
+            set { isEnemy = value; }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Color c = Color.blue;
+            if (isEnemy)
+                c = Color.red;
+
+            if (isEnemy)
+            {
+                GetComponent<MeshRenderer>().sharedMaterial.SetColor("_EmissionColor", c * 2);
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", c * 2);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
